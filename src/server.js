be4cc52d2f7f177d7990c963b09270456ef93cc4
Jhon Cruz <jhonskaz@gmail.com>
2024-04-06 10:35:12 -0300

    Adiciona tratativa de POST usando stream

--- src/server.js
+++ src/server.js
@@ -8,19 +8,36 @@ const users = [
   },
 ];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+
+  const buffers = [];
+
+  for await (const chunck of req) {
+    // conseguimos usar um await em um laço for para aguardar todas as chunks de uma req
+    buffers.push(chunck);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
+
   if (method === "GET" && url === "/users") {
     return res
       .setHeader("Content-Type", "application/json")
       .end(JSON.stringify(users));
   }
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
+
     users.push({
       id: 1,
-      name: "John Doe",
-      email: "johndoe@example.com",
+      name: name,
+      email: email,
     });
+
     return res.writeHead(201).end();
   }
   return res.writeHead(404).end();
