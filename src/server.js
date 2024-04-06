f7ea054fa8ef465cf68b5427934b1561a31f35b4
Jhon Cruz <jhonskaz@gmail.com>
2024-04-06 11:43:13 -0300

    Adiciona banco de dados local

--- src/server.js
+++ src/server.js
@@ -1,13 +1,8 @@
 import http from "node:http";
 import { json } from "./midleware/json.js";
+import { Database } from "./database.js";
 
-const users = [
-  {
-    id: 0,
-    name: "Jhon Cruz",
-    email: "jhoncruz@example.com",
-  },
-];
+const database = new Database();
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -15,16 +10,18 @@ const server = http.createServer(async (req, res) => {
   await json(req, res);
 
   if (method === "GET" && url === "/users") {
+    const users = database.select("users");
     return res.end(JSON.stringify(users));
   }
   if (method === "POST" && url === "/users") {
     const { name, email } = req.body;
 
-    users.push({
+    const user = {
       id: 1,
       name: name,
       email: email,
-    });
+    };
+    database.insert("users", user);
 
     return res.writeHead(201).end();
   }
