18574f815fc8dc7e31495cf1d51705ed85e8de14
Jhon Cruz <jhonskaz@gmail.com>
2024-04-08 10:35:38 -0300

    Adiciona separação das rotas

---
+++ src/routes.js
@@ -0,0 +1,30 @@
+const database = new Database();
+import { Database } from "./database.js";
+import { randomUUID } from "node:crypto";
+
+export const routes = [
+  {
+    method: "GET",
+    path: "/users",
+    handler: (req, res) => {
+      const users = database.select("users");
+      return res.end(JSON.stringify(users));
+    },
+  },
+  {
+    method: "POST",
+    path: "/users",
+    handler: (req, res) => {
+      const { name, email } = req.body;
+
+      const user = {
+        id: randomUUID(),
+        name: name,
+        email: email,
+      };
+      database.insert("users", user);
+
+      return res.writeHead(201).end();
+    },
+  },
+];
